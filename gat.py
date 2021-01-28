9b054b9b3e4b25211dd7b36ce5ef3f40b1dae27a
Clément Piat <clement.piat2@gmail.com>
2021-01-28 09:22:18 +0100

    gat: try quicker ways

--- gat.py
+++ gat.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 from torch.nn import Linear, Conv1d
 from torch import nn
 from tqdm import tqdm
+from time import time
 
 class AttentionGraphModel(nn.Module):
 
@@ -28,6 +29,13 @@ class AttentionGraphModel(nn.Module):
             self.add_attention_layer(hidden_size*n_head, hidden_size, n_head)
 
         self.add_attention_layer(hidden_size*n_head, output_size, n_head)
+    
+    def add_attention_layer(self, n_features, n_hidden_features, n_head):
+        linear = Linear(n_features, n_hidden_features)
+        attn_layer = Conv1d(n_hidden_features*2*n_head, n_head, kernel_size=1, groups=n_head)
+        
+        self.linears.append(linear)
+        self.attn_layers.append(attn_layer)
 
     def forward(self, inputs):
         outputs = inputs
@@ -35,42 +43,62 @@ class AttentionGraphModel(nn.Module):
             outputs = self.forward_attention(outputs, linear, attn_layer, final_layer=(i==self.n_layers))
         return outputs
 
-    def get_h2(self, attn_layer, h, indices):
+    def get_h2(self, attn_layer, h, indices, adjacency_list):
+        """
+        h2 is of size (n_nodes,n_hidden_features,n_head)
+        """
         n_nodes, n_features = h.shape
-        values = torch.zeros(len(indices[0]), self.n_head, device=self.device)
-        h_conv = h.repeat(1,self.n_head).view(n_nodes,1,n_features*self.n_head,1)
-        for k, (i,j) in enumerate(zip(indices[0], indices[1])):
-            values[k,:] = attn_layer(torch.cat((h_conv[i],h_conv[j]), dim=1)).view(-1)
+        n_edges = len(indices[0])
 
+        # values = torch.zeros(n_edges, self.n_head, device=self.device)
+        h_conv = h.repeat(1,self.n_head).view(n_nodes,1,n_features*self.n_head,1)
+        h_concat = torch.cat([torch.cat((h_conv[i],h_conv[j]), dim=1) for (i,j) in zip(indices[0], indices[1])], dim=0)
+        values = attn_layer(h_concat).view(n_edges, -1)
+        
+        # for k, (i,j) in enumerate(zip(indices[0], indices[1])):
+        #     values[k,:] = attn_layer(torch.cat((h_conv[i],h_conv[j]), dim=1)).view(-1)
         e = torch.sparse_coo_tensor(indices,values)
 
         alpha = torch.sparse.softmax(e, dim=1).coalesce()
         values = alpha.values()
+    
+        t = time()
         h2 = torch.zeros(n_nodes, n_features, self.n_head, device=self.device)
         for i,j,v in zip(indices[0], indices[1], values):
             h2[i,:,:] += h[j,:].unsqueeze(-1) @ v.unsqueeze(0)
+        print(time()-t)
+        t = time()
+        h2 = torch.zeros(n_nodes, n_features, self.n_head, device=self.device)
+        h = h.unsqueeze(2) # (n_nodes, n_features, 1)
+        values = values.unsqueeze(1) # (n_edges, 1, n_head)
+        for i in range(n_nodes):
+            h_neigh = torch.cat([h[j,:] for j in adjacency_list[i]], dim=1)
+            values_neigh = torch.cat([values[j,:] for j in adjacency_list[i]], dim=0)
+            h2[i] = h_neigh @ values_neigh
+        print(time()-t)
 
         return h2
 
     def forward_attention(self, x, linear, attn_layer, final_layer=False):
         h = F.leaky_relu(linear(x), negative_slope=0.2)
 
-        adjacency_matrix = self.g.adjacency_matrix().coalesce().to(self.device)
-        indices = adjacency_matrix.indices()
+        indices, adjacency_list = self.get_graph_structure()
 
-        """
-        h2 is of size (n_nodes,n_hidden_features,n_head)
-        """
-        h2 = self.get_h2(attn_layer, h, indices)
+        h2 = self.get_h2(attn_layer, h, indices, adjacency_list)
         
         if final_layer:
             return self.activation(h2.mean(dim=-1))
         else:
             return self.activation(h2.view(h2.size(0),-1))
 
-    def add_attention_layer(self, n_features, n_hidden_features, n_head):
-        linear = Linear(n_features, n_hidden_features)
-        attn_layer = Conv1d(n_hidden_features*2*n_head, n_head, kernel_size=1, groups=n_head)
-        
-        self.linears.append(linear)
-        self.attn_layers.append(attn_layer)
+
+    def get_graph_structure(self):
+        adjacency_matrix = self.g.adjacency_matrix().coalesce().to(self.device)
+        indices = adjacency_matrix.indices()
+        n_nodes = self.g.num_nodes()
+
+        adjacency_list = [[]] * n_nodes
+        for (i,j) in zip(indices[0], indices[1]):
+            adjacency_list[i].append(j)
+
+        return indices, adjacency_list
